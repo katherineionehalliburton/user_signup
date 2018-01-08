e94402809743897c40adfe2d5e78f049aa102f3f
Katherine Halliburton <katherineionehalliburton@gmail.com>
2018-01-08 15:32:46 -0500

    I have 0 parameters listed, I am just trying to get this to return my Welcome Page - initial form renders fine

---
+++ main.py
@@ -0,0 +1,40 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+form = """
+<!DOCTYPE HTML>
+<html>
+    <body>
+        <form action="/" Method=['POST']>
+            
+            <p><label for=username>Enter Username:</label>
+            <input id="username" type=text name="username:"></p>
+
+            <p><label for=password>Please enter password:</label>
+            <input id="password" type=password name="password"></p>
+
+            <p><label for=confirm_pass>Confirm Password:</label>
+            <input id="confirm_pass" type=password name="confirm_pass"></p>
+
+            <label for=email>Email:</label>
+            <input id="email" type=text name="email">            
+            <label for=checkbox>Select to receive email updates</label>
+            <input id="checkbox" type=checkbox name="checkbox"> 
+
+            <p><input id="submit" type=submit name="submit"></p>
+        
+        </form>
+    </body>
+</html>
+"""
+@app.route("/")
+def input_rules():
+    return form
+
+@app.route("/WelcomePage", methods=['POST', 'GET'])
+def welcome():
+    username = request.form['username']
+    return '<h1>Welcome, ' + username + '</h1>'
+app.run()
