77493bee720f59dbaab7b51972c48674ed385bd0
Katherine Halliburton <katherineionehalliburton@gmail.com>
2018-01-09 02:19:01 -0500

    first two templates and updated main.py file

--- main.py
+++ main.py
@@ -1,41 +1,23 @@
 from flask import Flask, request
+import cgi
+import os
+import jinja2
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir))
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
-form = """
-<!DOCTYPE HTML>
-<html>
-    <body>
-        <form action="/" Method='POST'>
-            
-            <p><label for=username>Enter username:</label>
-            <input id="username" type=text name="username"></p>
-
-            <p><label for=password>Please enter password:</label>
-            <input id="password" type=password name="password"></p>
-
-            <p><label for=confirm_pass>Confirm Password:</label>
-            <input id="confirm_pass" type=password name="confirm_pass"></p>
-
-            <label for=email>Email:</label>
-            <input id="email" type=text name="email">            
-            <label for=checkbox>Select to receive email updates</label>
-            <input id="checkbox" type=checkbox name="checkbox"> 
-
-            <p><input id="submit" type=submit name="submit"></p>
-        
-        </form>
-    </body>
-</html>
-"""
 @app.route("/")
 def user_signup():
-    return form
+    template = jinja_env.get_template('usersignup.html')
+    return template.render()
 
-@app.route("/WelcomePage", methods=['POST'])
+@app.route("/", methods=['POST'])
 def welcome():
     username = request.form['username']
-    return '<h1>Welcome, ' + username + '!</h1>'
+    template = jinja_env.get_template('welcome_page.html')
+    return template.render(username)
 
 app.run()
